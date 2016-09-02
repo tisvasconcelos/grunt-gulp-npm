0e09256680df0aa48c011cb19d76f22326263043
Tiago Vasconcelos <tisvasconcelos@gmail.com>
2016-09-02 13:06:21 -0300

    Adds simple examples and gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      options: {
+        force: true
+      },
+      folder: ['assets/build/']
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: [{
+          expand: true,
+          cwd: './assets/src/css',
+          src: ['*.scss'],
+          dest: './assets/build/css',
+          ext: '.css'
+        }]
+      }
+    },
+    uglify: {
+      build: {
+        files: [{
+           expand: true,
+           cwd: './assets/src/js',
+           src: '**/*.js',
+           dest: './assets/build/js'
+        }]
+      }
+    }
+  });
+
+  // Plugins
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Task(s).
+  grunt.registerTask('assets', ['sass:dist', 'uglify:build']);
+  grunt.registerTask('default', ['clean', 'assets']);
+};
---
+++ assets/src/js/index.js
@@ -0,0 +1,3 @@
+(function(window){
+  window.alert('Grunt?');
+})(window);
